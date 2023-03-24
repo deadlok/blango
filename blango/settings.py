94317082f6e1505627d5f3d25b50f551575c15eb
man lok lee <lok@pg95.com>
2023-03-24 00:26:50 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog'
 ]
 
@@ -140,3 +142,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
